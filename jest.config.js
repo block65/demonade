e4da5f4a88c02375dadd54be37f5db82c63907ef
Max Holman <max@holmn.com>
2023-01-14 19:23:46 +0800

    chore: remove redundant options

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 export default {
   modulePathIgnorePatterns: ['<rootDir>/dist'],
   testMatch: ['<rootDir>/__tests__/**/*.test.js'], // ts because they will be compiled by bazel
-  verbose: true,
-  moduleNameMapper: {
-    '^(\\..*)\\.jsx?$': '$1', // support for ts imports with .js extensions
-  },
 };
